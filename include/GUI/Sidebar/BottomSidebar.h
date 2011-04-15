210e60047654f47202226c562495342b733e9b4c
brunodea <brunodea@inf.ufsm.br>
2011-04-15 18:30:23 -0300

    BottomSidebar and SampleValueSpinner classes added.

---
+++ include/GUI/Sidebar/BottomSidebar.h
@@ -0,0 +1,16 @@
+#ifndef _CG_T1_BOTTOM_SIDEBAR_PANE_
+#define _CG_T1_BOTTOM_SIDEBAR_PANE_
+
+#include <SCV/Panel.h>
+
+namespace GUI
+{
+   class BottomSidebar : public scv::Panel
+   {
+   public:
+      BottomSidebar();
+
+   }; //end of class BottomSidebar
+} //end of namespace GUI.
+
+#endif
---
+++ include/GUI/Sidebar/SampleValueSpinner.h
@@ -0,0 +1,13 @@
+#ifndef _BRUNODEA_CG_T1_SAMPLE_VALUE_SPINNER_H_
+#define _BRUNODEA_CG_T1_SAMPLE_VALUE_SPINNER_H_
+
+#include <SCV/Spinner.h>
+
+namespace GUI
+{
+   class SampleValueSpinner : public scv::Spinner
+   {
+   }; //end of class SampleValueSpinner
+} //end of namespace GUI.
+
+#endif
