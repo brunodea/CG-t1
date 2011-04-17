c4a27cdbb5a0ba7ce37142be891ad32db0bb238b
brunodea <brunodea@inf.ufsm.br>
2011-04-17 13:19:28 -0300

    Position in the sample vector added to the SampleValueSpinner class

--- include/GUI/Sidebar/SampleValueSpinner.h
+++ include/GUI/Sidebar/SampleValueSpinner.h
@@ -2,6 +2,7 @@
 #define _BRUNODEA_CG_T1_SAMPLE_VALUE_SPINNER_H_
 
 #include <SCV/Spinner.h>
+#include <SCV/Point.h>
 #include "../../macros.h"
 
 namespace GUI
@@ -10,6 +11,16 @@ namespace GUI
    {
    public:
       SampleValueSpinner(int pos_x, int pos_y);
+      SampleValueSpinner(const scv::Point &p);
+
+      inline void setVecPos(int i) { m_iVecPos = i; }
+      inline int getVecPos() { return m_iVecPos; }
+
+   private:
+      int m_iVecPos; //position in the vector of sample value spinners.
+
+   private:
+      void onValueChange();
 
    }; //end of class SampleValueSpinner
 } //end of namespace GUI.
--- src/GUI/Sidebar/SampleValueSpinner.cpp
+++ src/GUI/Sidebar/SampleValueSpinner.cpp
@@ -1,10 +1,24 @@
 #include "../../../include/GUI/Sidebar/SampleValueSpinner.h"
 
 #include <SCV/Point.h>
+#include <iostream>
+
+#include "../../../include/GUI/Sidebar/BottomSidebar.h"
 
 using namespace GUI;
 
 SampleValueSpinner::SampleValueSpinner(int pos_x, int pos_y)
    : scv::Spinner(scv::Point(pos_x, pos_y), SAMPLE_SPINNER_WIDTH, SAMPLE_SPINNER_MIN_VALUE, SAMPLE_SPINNER_MAX_VALUE, 0, 1)
+{
+   setVecPos(0);
+}
+
+SampleValueSpinner::SampleValueSpinner(const scv::Point &p)
+   : scv::Spinner(p, SAMPLE_SPINNER_WIDTH, SAMPLE_SPINNER_MIN_VALUE, SAMPLE_SPINNER_MAX_VALUE, 0, 1)
+{
+   setVecPos(0);
+}
+
+void SampleValueSpinner::onValueChange()
 {
 }
