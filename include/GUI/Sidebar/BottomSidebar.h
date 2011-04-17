9cd833611e78ff1e89fb770e24251272889f0bf2
brunodea <brunodea@inf.ufsm.br>
2011-04-17 13:18:09 -0300

    BottomSidebar as Singleton.

--- include/GUI/Sidebar/BottomSidebar.h
+++ include/GUI/Sidebar/BottomSidebar.h
@@ -1,7 +1,12 @@
+/**
+  * BottomSidebar eh um panel que fica na parte inferior da tela. Nele ficam os spinners que representam
+  * a amostra. Como soh pode existir um BottomSidebar, ele eh singleton.
+ **/
 #ifndef _BRUNODEA_CG_T1_BOTTOM_SIDEBAR_PANE_
 #define _BRUNODEA_CG_T1_BOTTOM_SIDEBAR_PANE_
 
 #include <SCV/Panel.h>
+#include <SCV/Label.h>
 #include <vector>
 
 #include "SampleValueSpinner.h"
@@ -11,19 +16,23 @@ namespace GUI
    class BottomSidebar : public scv::Panel
    {
    public:
-      BottomSidebar(int pos_x, int pos_y);
+      static BottomSidebar *instance(); //Retorna a unica instancia da classe.
       ~BottomSidebar();
 
-      inline int getNumberOfSamples() { return m_iNumberOfSamples; }
-
-
-   private:
-      std::vector<SampleValueSpinner *> m_vSampleSpinners;
-      int m_iNumberOfSamples; //numero de amostras (8 ou 16).      
+      inline int getNumberOfSamples() { return m_iElementsInSample; }
 
    private:
+      static BottomSidebar *m_sInstance;
+
+      std::vector<SampleValueSpinner *> m_vSampleSpinners; //vetor com a amostra.
+      int m_iElementsInSample; //numero de elementos na amostra (8 ou 16).      
+
+      scv::Label *m_pSampleLabel;
+
+   private:
+      BottomSidebar(int pos_x, int pos_y); //contrutor privado pois se trata de um singleton.
       void initSampleSpinners();
-
+      void initGUIMembers();
 
    }; //end of class BottomSidebar
 } //end of namespace GUI.
--- src/GUI/Sidebar/BottomSidebar.cpp
+++ src/GUI/Sidebar/BottomSidebar.cpp
@@ -6,21 +6,31 @@
 
 using namespace GUI;
 
+BottomSidebar *BottomSidebar::m_sInstance = NULL;
+
 BottomSidebar::BottomSidebar(int pos_x, int pos_y)
-   : scv::Panel(scv::Point(pos_x, pos_y), WINDOW_WIDTH, WINDOW_HEIGHT / 6)
+   : scv::Panel(scv::Point(pos_x, pos_y), WINDOW_WIDTH, WINDOW_HEIGHT / 6),
+     m_iElementsInSample(16)
 {
-   initSampleSpinners();
+   initGUIMembers();
 }
 
 BottomSidebar::~BottomSidebar()
 {
+   delete m_pSampleLabel;
    for(unsigned int i = 0; i < m_vSampleSpinners.size(); i++)
       delete m_vSampleSpinners[i];
 }
 
+BottomSidebar *BottomSidebar::instance()
+{
+   if(m_sInstance == NULL)
+      m_sInstance = new BottomSidebar(0,  WINDOW_HEIGHT - (WINDOW_HEIGHT / 6));
+   return m_sInstance;
+}
+
 void BottomSidebar::initSampleSpinners()
 {
-   int number_of_initial_samples = 16;
    int y = 0;
    int x;
    int offset_x = SAMPLE_SPINNER_WIDTH / 2;
@@ -28,7 +38,7 @@ void BottomSidebar::initSampleSpinners()
    int offset_y = 23;
 
    int col = 0;
-   for(int i = 0; i < number_of_initial_samples; i++)
+   for(int i = 0; i < m_iElementsInSample; i++)
    {
       if(i % 8 == 0)
       {
@@ -37,7 +47,7 @@ void BottomSidebar::initSampleSpinners()
       }
       x = col*SAMPLE_SPINNER_WIDTH + offset_x;
       SampleValueSpinner *spinner = new SampleValueSpinner(x, y);
-      
+      spinner->setVecPos(i);
       this->addComponent(spinner);
       m_vSampleSpinners.push_back(spinner);
 
@@ -45,3 +55,12 @@ void BottomSidebar::initSampleSpinners()
    }
 }
 
+void BottomSidebar::initGUIMembers()
+{
+   m_pSampleLabel = new scv::Label(scv::Point(10, 5), "Sample Vector:");
+   this->addComponent(m_pSampleLabel);
+   
+   initSampleSpinners();
+
+   
+}
