27401d0f823bba4eaca356002466cca9b900e227
Bruno Azevedo <brunodea@inf.ufsm.br>
2011-04-20 14:40:32 -0300

    Class SampleSpinnerPanel added.

---
+++ include/GUI/Sidebar/SampleSpinnersPanel.h
@@ -0,0 +1,21 @@
+#ifndef _BRUNODEA_CG_T1_SAMPLE_SPINNERS_PANE_H_
+#define _BRUNODEA_CG_T1_SAMPLE_SPINNERS_PANE_H_
+
+#include <SCV/Panel.h>
+#include <vector>
+#include "GUI/Sidebar/SampleValueSpinner.h"
+
+namespace GUI
+{
+	class SampleSpinnersPanel : public scv::Panel
+	{
+	public:
+		SampleSpinnersPanel(const scv::Panel &panel);
+
+	private:
+		std::vector<std::vector<SampleValueSpinner> *> m_vvSpinners;
+
+	}; //end of class SampleSpinnersPanel.
+} //end of namespace GUI.
+
+#endif
---
+++ src/GUI/Sidebar/SampleSpinnersPanel.cpp
@@ -0,0 +1,8 @@
+#include "GUI/Sidebar/SampleSpinnersPanel.h"
+
+using namespace GUI;
+
+SampleSpinnersPanel::SampleSpinnersPanel(const scv::Panel &p)
+	: scv::Panel(p)
+{
+}
