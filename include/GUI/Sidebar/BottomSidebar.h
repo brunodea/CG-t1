adf633e59f904babc0dfbe3be503f8a9e36ab050
Bruno Azevedo <brunodea@inf.ufsm.br>
2011-04-20 14:59:26 -0300

    Vector of SampleValueSpinner removed from BottomSidebar

--- include/GUI/Sidebar/BottomSidebar.h
+++ include/GUI/Sidebar/BottomSidebar.h
@@ -20,17 +20,12 @@ namespace GUI
    public:
       static BottomSidebar *instance(); //Retorna a unica instancia da classe.
       ~BottomSidebar();
-
-      inline int getNumberOfSamples() { return m_iElementsInSample; }
-
+      
       void generateRandomSample();
       void generateLinearSample();
 
    private:
-      static BottomSidebar *m_sInstance;
-
-      std::vector<SampleValueSpinner *> m_vSampleSpinners; //vetor com a amostra.
-      int m_iElementsInSample; //numero de elementos na amostra (8 ou 16).      
+      static BottomSidebar *m_sInstance;     
 
       std::vector<GenerateSampleButton *> m_vGenerateSampleButtons; //vetor com os botoes para geracao automatica da amostra.
 
@@ -39,7 +34,6 @@ namespace GUI
 
    private:
       BottomSidebar(int pos_x, int pos_y); //contrutor privado pois se trata de um singleton.
-      void initSampleSpinners();
       void initGenerateSampleButtons();
       void initGUIMembers();
 
--- src/GUI/Sidebar/BottomSidebar.cpp
+++ src/GUI/Sidebar/BottomSidebar.cpp
@@ -11,8 +11,7 @@ using namespace GUI;
 BottomSidebar *BottomSidebar::m_sInstance = NULL;
 
 BottomSidebar::BottomSidebar(int pos_x, int pos_y)
-   : scv::Panel(scv::Point(pos_x, pos_y), WINDOW_WIDTH, WINDOW_HEIGHT / 6),
-     m_iElementsInSample(16)
+   : scv::Panel(scv::Point(pos_x, pos_y), WINDOW_WIDTH, WINDOW_HEIGHT / 6)
 {
    initGUIMembers();
 }
@@ -21,8 +20,6 @@ BottomSidebar::~BottomSidebar()
 {
    delete m_pSeparator;
    delete m_pSampleLabel;
-   for(unsigned int i = 0; i < m_vSampleSpinners.size(); i++)
-      delete m_vSampleSpinners[i];
    for(unsigned int i = 0; i < m_vGenerateSampleButtons.size(); i++)
       delete m_vGenerateSampleButtons[i];
 }
@@ -32,32 +29,6 @@ BottomSidebar *BottomSidebar::instance()
    if(m_sInstance == NULL)
       m_sInstance = new BottomSidebar(0,  WINDOW_HEIGHT - (WINDOW_HEIGHT / 6));
    return m_sInstance;
-}
-
-void BottomSidebar::initSampleSpinners()
-{
-   int y = 0;
-   int x;
-   int offset_x = SAMPLE_SPINNER_WIDTH / 2;
-
-   int offset_y = 23;
-
-   int col = 0;
-   for(int i = 0; i < m_iElementsInSample; i++)
-   {
-      if(i % 8 == 0)
-      {
-         y += offset_y;
-         col = 0;
-      }
-      x = col*SAMPLE_SPINNER_WIDTH + offset_x;
-      SampleValueSpinner *spinner = new SampleValueSpinner(x, y);
-      spinner->setVecPos(i);
-      this->addComponent(spinner);
-      m_vSampleSpinners.push_back(spinner);
-
-      col++;
-   }
 }
 
 void BottomSidebar::initGenerateSampleButtons()
@@ -82,7 +53,6 @@ void BottomSidebar::initGUIMembers()
    this->addComponent(m_pSampleLabel);
    this->addComponent(m_pSeparator);
 
-   initSampleSpinners();
    initGenerateSampleButtons();
    
 }
