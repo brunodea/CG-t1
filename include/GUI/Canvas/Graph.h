b300a29a2d9e1fdc1640a5d7cc4632041894682c
Bruno Azevedo <brunodea@inf.ufsm.br>
2011-04-21 20:19:27 -0300

    Graph draws the axis.

--- include/GUI/Canvas/Graph.h
+++ include/GUI/Canvas/Graph.h
@@ -1,15 +1,35 @@
 #ifndef _BRUNODEA_CG_T1_GRAPH_H_
 #define _BRUNODEA_CG_T1_GRAPH_H_
 
+#include <SCV/Canvas.h>
+#include <SCV/Point.h>
+#include <vector>
+#include "GUI/Canvas/GraphPoint.hpp"
+
 namespace GUI
 {
    class Graph
    {
    public:
-      Graph();
+      Graph(const scv::Point &p0x0, unsigned int xLenght, unsigned int yLength);
+      ~Graph();
+
+      void addPoints(const std::vector<GraphPoint *> &points);
+      void draw();
+   public:
+      scv::Point m_Pos0x0;
+      unsigned int m_XLength;
+      unsigned int m_YLength;
+
+   private:
+      std::vector<GraphPoint *> *m_vpPoints;
+
+
+   private:
+      void drawCoordinates();
 
    }; //end of class Graph.
 
 } //end of namespace GUI.
 
-#define
+#endif
--- src/GUI/Canvas/Graph.cpp
+++ src/GUI/Canvas/Graph.cpp
@@ -2,7 +2,58 @@
 
 using namespace GUI;
 
-Graph::Graph()
+Graph::Graph(const scv::Point &p0x0, unsigned int xLength, unsigned int yLength)
+   : m_Pos0x0(p0x0), m_XLength(xLength), m_YLength(yLength)
 {
+   m_vpPoints = new std::vector<GraphPoint *>();
+}
+
+Graph::~Graph()
+{
+   for(unsigned int i = 0; i < m_vpPoints->size(); i++)
+      delete m_vpPoints->at(i);
+   delete []m_vpPoints;
+}
+
+void Graph::addPoints(const std::vector<GraphPoint *> &points)
+{
+   m_vpPoints->insert(m_vpPoints->end(), points.begin(), points.end());
+}
+
+void Graph::drawCoordinates()
+{
+   glBegin(GL_LINES);
+      //draw coordinate x
+      glColor4f(0.f, 0.f, 0.f, 1.f);
+      glVertex2i(m_Pos0x0.x, m_Pos0x0.y);
+      glVertex2i(m_Pos0x0.x + m_XLength, m_Pos0x0.y);
+
+      //draw x arrow
+      glVertex2i(m_Pos0x0.x + m_XLength - 10, m_Pos0x0.y - 5);
+      glVertex2i(m_Pos0x0.x + m_XLength, m_Pos0x0.y);
+
+      glVertex2i(m_Pos0x0.x + m_XLength - 10, m_Pos0x0.y + 5);
+      glVertex2i(m_Pos0x0.x + m_XLength, m_Pos0x0.y);
+
+
+      //draw coordinate y
+      glVertex2i(m_Pos0x0.x, m_Pos0x0.y);
+      glVertex2i(m_Pos0x0.x, m_Pos0x0.y - m_YLength);
+
+      //draw y arrow
+      glVertex2i(m_Pos0x0.x - 5, m_Pos0x0.y - m_YLength + 10);
+      glVertex2i(m_Pos0x0.x, m_Pos0x0.y - m_YLength);
+      
+      glVertex2i(m_Pos0x0.x + 5, m_Pos0x0.y - m_YLength + 10);
+      glVertex2i(m_Pos0x0.x, m_Pos0x0.y - m_YLength);
+
+
+   glEnd();
+}
+
+void Graph::draw()
+{
+   drawCoordinates();
+
 }
 
