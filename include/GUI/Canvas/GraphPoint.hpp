b9b16cc09b479b60be117d8881fcffd1cbee7e27
brunodea <brunodea@inf.ufsm.br>
2011-04-24 15:58:28 -0300

    GraphPoint.hpp commented.

--- include/GUI/Canvas/GraphPoint.hpp
+++ include/GUI/Canvas/GraphPoint.hpp
@@ -1,3 +1,7 @@
+/*
+ * Classe que representa um ponto no gráfico.
+ */
+
 #ifndef _BRUNODEA_CG_T1_GRAPH_POINT_H_
 #define _BRUNODEA_CG_T1_GRAPH_POINT_H_
 
@@ -15,16 +19,28 @@ namespace GUI
    class GraphPoint
    {
    public:
+      /*
+       * Construtor.
+       * p: posição na canvas em que o ponto vai ficar.
+       */
       GraphPoint(const scv::Point &p)
          : m_Pos(p), m_Color(0.f, 0.5f, 0.f), m_iHalfWidth(GRAPH_POINT_WIDTH/2), 
            m_iHalfHeight(GRAPH_POINT_HEIGHT/2)           
       {}
 
+      /*
+       * Construtor.
+       * p: posição na canvas em que o ponto vai ficar.
+       * c: sua cor.
+       */
       GraphPoint(const scv::Point &p, const scv::Color4f &c)
          : m_Pos(p), m_Color(c), m_iHalfWidth(GRAPH_POINT_WIDTH/2), 
            m_iHalfHeight(GRAPH_POINT_HEIGHT/2)
       {}
 
+      /*
+       * Desenha o ponto (na verdade desenha um retângulo representando o ponto).
+       */
       inline void draw()
       {
          glBegin(GL_QUADS);
@@ -36,21 +52,31 @@ namespace GUI
          glEnd();
       }
 
+      /*
+       * Verifica se a posição 'pt' está dentro do (retângulo que representa o) ponto.
+       * pt: posição que se quer fazer a verificação.
+       */
       inline bool isInside(const scv::Point &pt)
       {
-         if(pt.x >= m_Pos.x - m_iHalfWidth && pt.x <= m_Pos.x + m_iHalfWidth && pt.y >= m_Pos.y - m_iHalfHeight && pt.y <= m_Pos.y + m_iHalfHeight)
+         if(pt.x >= m_Pos.x - m_iHalfWidth && pt.x <= m_Pos.x + m_iHalfWidth &&
+            pt.y >= m_Pos.y - m_iHalfHeight && pt.y <= m_Pos.y + m_iHalfHeight)
             return true;
          return false;
       }
 
    public:
-      scv::Point m_Pos;
+      scv::Point m_Pos; //posição do ponto na canvas.
 
    private:
-      scv::Color4f m_Color;
+      scv::Color4f m_Color; //cor do ponto.
 
-      int m_iHalfWidth;
-      int m_iHalfHeight;
+      /* 
+       * É a metade para não ter que ficar fazendo a divisão por 2 o tempo todo.
+       * E é assim para que a posição m_Pos sempre fique no meio do retângulo.
+       */
+      int m_iHalfWidth; //metade da largura do retângulo que representa o ponto.
+      int m_iHalfHeight; //metade da altura do retângulo que representa o ponto.
+
    }; //end of class GraphPoint.
 } //end of namespace GUI.
 
