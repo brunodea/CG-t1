d5e8948bccac082e53f98fd1711de51dfe9f8e63
brunodea <brunodea@inf.ufsm.br>
2011-04-24 16:09:55 -0300

    GraphsCanvas.h and GraphsCanvas.cpp commented.

--- include/GUI/Canvas/GraphsCanvas.h
+++ include/GUI/Canvas/GraphsCanvas.h
@@ -1,3 +1,6 @@
+/*
+ * Canvas onde os gráficos dos sinais originais, da fdct e da idct serão desenhados.
+ */
 #ifndef _BRUNODEA_CG_T1_GRAPHS_CANVAS_H_
 #define _BRUNODEA_CG_T1_GRAPHS_CANVAS_H_
 
@@ -16,9 +19,14 @@ namespace GUI
    class GraphsCanvas : public scv::Canvas
    {
    public:
+      /*
+       * Construtor.
+       * p: posição na tela onde a canvas vai ser posicionada.
+       */
       GraphsCanvas(const scv::Point &p);
       ~GraphsCanvas();
 
+      /* Getters. */
       inline Graph *getOrigSampleGraph() { return m_pOrigSampleGraph; }
       inline Graph *getFDCTGraph() { return m_pFDCTGraph; }
       inline Graph *getIDCTGraph() { return m_pIDCTGraph; }
@@ -26,19 +34,20 @@ namespace GUI
       void render();
       void update();
    protected:
+      /* Callbacks. */
       void onMouseClick(const scv::MouseEvent &evt);
       void onMouseWheel(const scv::MouseEvent &evt);
       void onKeyPressed(const scv::KeyEvent &evt);
       void onMouseOver(const scv::MouseEvent &evt);
 
    private:
-      scv::Color4f m_bgColor;
-      Graph *m_pOrigSampleGraph;
-      Graph *m_pFDCTGraph;
-      Graph *m_pIDCTGraph;
+      scv::Color4f m_bgColor; //cor do fundo da canvas.
+      Graph *m_pOrigSampleGraph; //gráfico que representa a amostra original.
+      Graph *m_pFDCTGraph; //gráfico que representa a amostra após a FDCT.
+      Graph *m_pIDCTGraph; //gráfico que representa a amostra após a IDCT.
 
-      GraphPoint *m_pCurrPointOvered; //current point that is overed by the mouse;
-      unsigned int m_iCurrPointIndex; //index of the current point overed by the mouse;
+      GraphPoint *m_pCurrPointOvered; //ponto atual que está com o mouse sobre ele.
+      unsigned int m_iCurrPointIndex; //índice do ponto atual em que o mouse está sobre.
 
    }; //end of class GraphsCanvas.
 } //end of namespace GUI.
--- src/GUI/Canvas/GraphsCanvas.cpp
+++ src/GUI/Canvas/GraphsCanvas.cpp
@@ -29,6 +29,7 @@ GraphsCanvas::~GraphsCanvas()
 
 void GraphsCanvas::update()
 {
+   /* Inicializa os gráficos. */
    if(m_pOrigSampleGraph == NULL)
        m_pOrigSampleGraph = new Graph(DCTVIEWER->getSignals(), "Original", scv::Point(10,450), 300, 360);
    if(m_pFDCTGraph == NULL)
@@ -36,16 +37,26 @@ void GraphsCanvas::update()
    if(m_pIDCTGraph == NULL)
       m_pIDCTGraph = new Graph(DCTVIEWER->getIDCTSignals(), "IDCT apos a FDCT", scv::Point(0,450), 300, 360);
 
+   /* 
+    * Ajuste da posição y do 0x0 de cada gráfico. Isso é necessário pois a escala pode aumentar o tamanho da canvas.
+    * Com isso os gráficos sempre ficam na parte de baixo da canvas.
+    */
    double height = getHeight() - 30;
    m_pOrigSampleGraph->m_Pos0x0.y = height;
    m_pFDCTGraph->m_Pos0x0.y = height;
    m_pIDCTGraph->m_Pos0x0.y = height;
 
+   /*
+    * Sempre seta os signals pois o método da DCTViewer "getFDCTSignals()" sempre traz os sinais atualizados.
+    */
    m_pFDCTGraph->setSignals(DCTVIEWER->getFDCTSignals());
+
+   /* Ajusta a posição x do 0x0 de acordo com a escala. */
    m_pFDCTGraph->m_Pos0x0.x = m_pOrigSampleGraph->m_Pos0x0.x +
       m_pOrigSampleGraph->getScale()*m_pOrigSampleGraph->m_XLength + 60;
    m_pFDCTGraph->setScale(m_pOrigSampleGraph->getScale());
 
+   /* Mesmo da FDCT. */
    m_pIDCTGraph->setSignals(DCTVIEWER->getIDCTSignals());
    m_pIDCTGraph->m_Pos0x0.x = m_pFDCTGraph->m_Pos0x0.x +
       m_pFDCTGraph->getScale()*m_pFDCTGraph->m_XLength + 60;
@@ -56,11 +67,14 @@ void GraphsCanvas::render()
 {   
    glClearColor(m_bgColor[0], m_bgColor[1], m_bgColor[2], m_bgColor[3]);
    glClear(GL_COLOR_BUFFER_BIT);
+
+   /* Faz o desenho dos gráficos. */
    m_pOrigSampleGraph->draw();
    m_pFDCTGraph->draw();
    m_pIDCTGraph->draw();
 
 
+   /* Caso o mouse esteja sobre um ponto, logo acima dele é desenhado seu index. */
    if(m_pCurrPointOvered != NULL && m_iCurrPointIndex >= 0)
    {
       glColor4f(0.f, 0.f, 6.f, 1.f);
@@ -77,6 +91,7 @@ void GraphsCanvas::render()
 
 void GraphsCanvas::onMouseWheel(const scv::MouseEvent &evt)
 {
+   /* Ajusta a escala quando o usuário mover a roda do mouse para cima ou para baixo. */
    if(evt.getState() == evt.wheelup)
    {
       double new_scale = m_pOrigSampleGraph->getScale() + 0.1;
@@ -93,6 +108,7 @@ void GraphsCanvas::onMouseWheel(const scv::MouseEvent &evt)
 
 void GraphsCanvas::onKeyPressed(const scv::KeyEvent &evt)
 {
+   /* Ajusta a escala quando o usuário apertar + ou -. */
    if(evt.getKeyCode() == '+')
    {
       double new_scale = m_pOrigSampleGraph->getScale() + 0.1;
@@ -109,25 +125,22 @@ void GraphsCanvas::onKeyPressed(const scv::KeyEvent &evt)
 
 void GraphsCanvas::onMouseClick(const scv::MouseEvent &evt)
 {
+   /*
+    * Quando o usuário clica com o botão do meio do mouse em cima de um ponto
+    * pertencente ao gráfico da FDCT ele é setado para zero (truncamento).
+    * E o gráfico da IDCT, bem como o vetor de sinais, é ajustado na mesma hora.
+    */
    if(evt.getButton() == evt.middle)
    {
       int index = -1;
-      /*
-      aux = m_pOrigSampleGraph->getPointAt(evt.getPosition());
-      if(aux != NULL)
-      {
-         std::cout << "onMouseClick worked for Original Sample Graph point\n";
-         m_pOrigSampleGraph->goToZero(aux);
-         return;
-      }*/
       index = m_pFDCTGraph->getPointAt(evt.getPosition());
       if(index >= 0)
       {
          GraphPoint *aux = m_pFDCTGraph->getPointAt(index);
          m_pFDCTGraph->goToZero(aux);
          
-         int row = index/8;
-         int col = index%8;
+         int row = index/MASTER_VALUE;
+         int col = index%MASTER_VALUE;
 
          double *val = &DCTVIEWER->getFDCTSignals()->at(row)->at(col);
          *val = 0;
@@ -136,18 +149,12 @@ void GraphsCanvas::onMouseClick(const scv::MouseEvent &evt)
          DCTVIEWER->getGraphsCanvas()->getIDCTGraph()->adjustPoints();
          return;
       }
-      /*
-      aux = m_pIDCTGraph->getPointAt(evt.getPosition());
-      if(aux != NULL)
-      {
-         m_pIDCTGraph->goToZero(aux);
-         return;
-      }*/
    }
 }
 
 void GraphsCanvas::onMouseOver(const scv::MouseEvent &evt)
 {
+   /* Ajusta o m_pCurrPointOvered e o m_iCurrPointIndex. */
    int index = -1;
    GraphPoint *aux = NULL;
    scv::Point pos;
